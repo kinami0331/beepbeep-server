baac263473fc00c4086c4fa02cf66a98d8296e49
kinami <sunhaonan331@outlook.com>
2021-07-04 14:06:33 +0800

    重新整理了matlab代码结构

---
+++ matlab/src/utils/calcDistance.m
@@ -0,0 +1,6 @@
+function out = calcDistance(receivedFile1, receivedFile2, originFile, m2s1, m2s2, Fs, fBegin, fEnd, soundSpeed)
+%CALCDISTANCE 此处显示有关此函数的摘要
+%   此处显示详细说明
+out = calc_distance(receivedFile1, receivedFile2, originFile, m2s1, m2s2, Fs, fBegin, fEnd, soundSpeed);
+end
+
