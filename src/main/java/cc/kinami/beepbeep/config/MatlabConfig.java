ba410a5d90b501317589bbe2d0a68e0a30219b21
kinami <sunhaonan331@outlook.com>
2021-07-12 21:49:27 +0800

    添加了启动matlab计算的开关，之后可能会添加python版本的计算实现

--- src/main/java/cc/kinami/beepbeep/config/MatlabConfig.java
+++ src/main/java/cc/kinami/beepbeep/config/MatlabConfig.java
@@ -1,6 +1,7 @@
 package cc.kinami.beepbeep.config;
 
 import cc.kinami.beepbeep.util.MatlabUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -8,9 +9,21 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class MatlabConfig {
 
+    static boolean ENABLE_MATLAB;
+    static boolean enableMatlab;
+    @Value("${beepbeep.enable-matlab}")
+    String enableMatlabConfig;
+
+    static boolean isEnableMatlab() {
+        return enableMatlab;
+    }
+
     @PostConstruct
     public void initMatlab() {
+        enableMatlab = enableMatlabConfig.equals("true");
 
+        if (!enableMatlab)
+            return;
         try {
             MatlabUtil matlabUtil = new MatlabUtil();
             matlabUtil.initMcr();
